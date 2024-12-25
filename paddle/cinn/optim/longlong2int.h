b10c109317af11ac6ac309043c9ff1a7a4efc77b
He Yang <1003844689@qq.com>
2024-12-25 03:33:24 +0000

    add comment on longlong2int.h

--- paddle/cinn/optim/longlong2int.h
+++ paddle/cinn/optim/longlong2int.h
@@ -18,6 +18,37 @@
 namespace cinn {
 namespace optim {
 
+/**
+ *
+ * This pass ensures that operations using `int64_t` in IR are cast down to
+ * `int32_t` where overflow is not a risk.
+ *
+ * This pass is applicable in scenarios where computations in a loop or tensor
+ * manipulation rely on 64-bit integer types (`int64_t`), but the operations are
+ * safe within 32-bit integer limits (`int32_t`). It is especially useful in
+ * architectures or compilers where using 32-bit integers yields better
+ * performance or reduces memory overhead. For example, large-scale tensor
+ * computations or loop iterations that would safely fit within the range of a
+ * 32-bit integer can benefit from this pass.
+ *
+ * When applied, this pass modifies the IR by:
+ * - Casting loop bounds (`min` and `extent`) and variable bounds from `int64_t`
+ * to `int32_t`.
+ * - Adjusting tensor shape dimensions and buffer metadata from `int64_t` to
+ * `int32_t`.
+ * - Ensuring all index expressions used in `Load` and `Store` operations are
+ * converted to `int32_t`.
+ * - Traversing schedule blocks and their variables, converting types from
+ * `int64_t` to `int32_t` where applicable.
+ *
+ * Performance impact: This pass addresses potential inefficiencies due to the
+ * use of 64-bit integers on architectures optimized for 32-bit operations. It
+ * can reduce memory usage and improve computation speed by minimizing the size
+ * of integers processed in critical sections like loops or tensor access
+ * patterns.
+ *
+ */
+
 // Try to change the type of longlong to int in the expr.
 void TryCastLonglong2Int(Expr* expr);
 }  // namespace optim
